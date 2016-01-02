30226b29e2529cac4df94b9d1ea30e1eaec33249
Erick Engelhardt <erickne@gmail.com>
2016-01-02 02:07:33 -0200

    Update angular-csv-import.js

--- dist/angular-csv-import.js
+++ dist/angular-csv-import.js
@@ -18,7 +18,9 @@ csvImport.directive('ngCsvImport', function() {
 			result: '=?',
 			encoding: '=?',
 			encodingVisible: '=?',
-			accept: '=?'
+			accept: '=?',
+            ignoreInitialLines: '=?',
+            ignoreFinalLines: '=?'
 		},
 		template: '<div>'+
 		  '<div ng-show="headerVisible"><div class="label">Header</div><input type="checkbox" ng-model="header"></div>'+
@@ -29,9 +31,16 @@ csvImport.directive('ngCsvImport', function() {
 			'</div>'+
 			'<div><input class="btn cta gray" type="file" multiple accept="{{accept}}"/></div>'+
 			'</div>',
+        controller: function($scope) {
+
+            console.log("controller",$scope);
+        },
 		link: function(scope, element) {
+            console.log("link",scope);
 			scope.separatorVisible = scope.separatorVisible || false;
 			scope.headerVisible = scope.headerVisible || false;
+            scope.ignoreInitialLines = scope.ignoreInitialLines || 0;
+            scope.ignoreFinalLines = scope.ignoreFinalLines || 0;
 
 			angular.element(element[0].querySelector('.separator-input')).on('keyup', function(e) {
 				if ( scope.content != null ) {
@@ -39,12 +48,57 @@ csvImport.directive('ngCsvImport', function() {
 						csv: scope.content,
 						header: scope.header,
 						separator: e.target.value,
-						encoding: scope.encoding
+						encoding: scope.encoding,
+                        ignoreInitialLines: scope.ignoreInitialLines,
+                        ignoreFinalLines: scope.ignoreFinalLines
 					};
-					scope.result = csvToJSON(content);
+					scope.result = scope.csvToJSON(content);
 					scope.$apply();
 				}
 			});
+
+
+            scope.csvToJSON = function(content) {
+
+                var lines=content.csv.split('\n');
+                var result = [];
+                var start = 0;
+                var columnCount = lines[0].split(content.separator).length;
+
+                console.log("ignoreInitialLines", content.ignoreInitialLines);
+                console.log("ignoreFinalLines", content.ignoreFinalLines);
+
+                for (i = 0; i < content.ignoreInitialLines ; i++) {
+                    lines.shift();
+                }
+                for (i = 0; i < content.ignoreFinalLines ; i++) {
+                    lines.pop();
+                }
+
+                var headers = [];
+                if (content.header) {
+                    headers=lines[0].split(content.separator);
+                    start = 1;
+                }
+
+                for (var i=start; i<lines.length; i++) {
+                    var obj = {};
+                    var currentline=lines[i].split(new RegExp(content.separator+'(?![^"]*"(?:(?:[^"]*"){2})*[^"]*$)'));
+                    //if ( currentline.length === columnCount ) {
+                    if (content.header) {
+                        for (var j=0; j<headers.length; j++) {
+                            obj[headers[j]] = currentline[j];
+                        }
+                    } else {
+                        for (var k=0; k<currentline.length; k++) {
+                            obj[k] = currentline[k];
+                        }
+                    }
+                    result.push(obj);
+                    //}
+                }
+                return result;
+            };
 
 			element.on('change', function(onChangeEvent) {
 				var reader = new FileReader();
@@ -54,10 +108,12 @@ csvImport.directive('ngCsvImport', function() {
 						var content = {
 							csv: onLoadEvent.target.result.replace(/\r\n|\r/g,'\n'),
 							header: scope.header,
-							separator: scope.separator
+							separator: scope.separator,
+                            ignoreInitialLines: scope.ignoreInitialLines,
+                            ignoreFinalLines: scope.ignoreFinalLines
 						};
 						scope.content = content.csv;
-						scope.result = csvToJSON(content);
+						scope.result = scope.csvToJSON(content);
 						scope.result.filename = scope.filename;
 					});
 				};
@@ -69,43 +125,16 @@ csvImport.directive('ngCsvImport', function() {
 						var content = {
 							csv: scope.content,
 							header: !scope.header,
-							separator: scope.separator
+							separator: scope.separator,
+                            ignoreInitialLines: scope.ignoreInitialLines,
+                            ignoreFinalLines: scope.ignoreFinalLines
 						};
-						scope.result = csvToJSON(content);
+						scope.result = scope.csvToJSON(content);
 					}
 				}
 			});
 
-			var csvToJSON = function(content) {
-				var lines=content.csv.split('\n');
-				var result = [];
-				var start = 0;
-				var columnCount = lines[0].split(content.separator).length;
 
-				var headers = [];
-				if (content.header) {
-					headers=lines[0].split(content.separator);
-					start = 1;
-				}
-
-				for (var i=start; i<lines.length; i++) {
-					var obj = {};
-					var currentline=lines[i].split(new RegExp(content.separator+'(?![^"]*"(?:(?:[^"]*"){2})*[^"]*$)'));
-					if ( currentline.length === columnCount ) {
-						if (content.header) {
-							for (var j=0; j<headers.length; j++) {
-								obj[headers[j]] = currentline[j];
-							}
-						} else {
-							for (var k=0; k<currentline.length; k++) {
-								obj[k] = currentline[k];
-							}
-						}
-						result.push(obj);
-					}
-				}
-				return result;
-			};
 		}
 	};
 });
